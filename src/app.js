24a558978175ec2033a541b36b912fadf835c943
Maribau11 <maribau11@gmail.com>
2023-07-10 15:02:02 +0200

    retrieving the current temperature using openWeather Api

--- src/app.js
+++ src/app.js
@@ -20,3 +20,15 @@ let minute = current.getMinutes();
 let hours = current.getHours();
 let h3Time = document.querySelector("#current-time");
 h3Time.innerHTML = `${hours}:${minute}`;
+
+function getResponse(response) {
+  let currentTemp = Math.round(response.data.main.temp);
+  let h3Temp = document.querySelector("#current-temp");
+
+  h3Temp.innerHTML = `${currentTemp}°C`;
+  console.log(response.data);
+}
+let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+let city = "Cape Town"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(`${apiUrl}`).then(getResponse);
