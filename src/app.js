4639ff2f87d6abad7648001e8a2023c8e6d9e242
Maribau11 <maribau11@gmail.com>
2023-07-10 14:32:13 +0200

    Changing #current-date to the actual date

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let current = new Date();
+let months = ["January","February", "March","April","May","June","July","August",
+"September","November","December"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = current.getDate();
+let month = months[current.getMonth()];
+let h3Day = document.querySelector("#current-day");
+h3Day.innerHTML = `${days[current.getDay()]}, ${day} ${month}`;
+
