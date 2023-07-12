13d517942e964031bdf82b8f9577437ce5fec177
Maribau11 <maribau11@gmail.com>
2023-07-12 13:53:21 +0200

    completed the code for the search bar and the conversions from c to f

--- src/app.js
+++ src/app.js
@@ -18,68 +18,133 @@ h3Day.innerHTML = `${days[current.getDay()]}, ${day} ${month}`;
 let minute = current.getMinutes();
 let hours = current.getHours();
 let h3Time = document.querySelector("#current-time");
-h3Time.innerHTML = `${hours}:${minute}`;
-
-function getWeather(response) {
-  let currentTemp = Math.round(response.data.main.temp);
-  let h3Temp = document.querySelector("#current-temp");
-
-  h3Temp.innerHTML = `${currentTemp}°C`;
-  console.log(response.data);
-
-  let windspeed = document.querySelector("#wind");
-  let humidity = document.querySelector("#hum");
-  let highTemp = document.querySelector("#hi");
-  let lowTemp = document.querySelector("#low");
-
-  let currentWindspeed = Math.round(response.data.wind.speed);
-  let currentHumidity = response.data.main.humidity;
-  let currentHighTemp = Math.round(response.data.main.temp_max); 
-  let currentLowTemp = Math.round(response.data.main.temp_min);
-
-  windspeed.innerHTML = `<strong>Wind</strong>: ${currentWindspeed}km/hr`;
-  humidity.innerHTML = `<strong>Humidy</strong>: ${currentHumidity}%`;
-  highTemp.innerHTML = `<strong>High</strong>: ${currentHighTemp}°C`;
-  lowTemp.innerHTML = `<strong>Low</strong>: ${currentLowTemp}°C`;
-
-  let icon = response.data.weather[0].icon;
-  const div = document.querySelector("#weather-icon");
-  const img = document.createElement("img");
-  const div2 = document.querySelector(".weather-app");
-  if (icon == "01d") {
-    img.setAttribute("src", "media/icon/sunny.png");
-    div2.style.backgroundImage = "url('media/background/sunnyDay.jpg')";
-    
-  } else if (icon == "02d") {
-    img.setAttribute("src", "media/icon/sunny-cloud.png");
-    div2.style.backgroundImage = "url('media/background/cloudyDay.jpg')";
-  } else if (icon == "03d") {
-    img.setAttribute("src", "media/icon/cloud.png");
-    div2.style.backgroundImage = "url('media/background/partlyCloudyDay.jpg')";
-  } else if (icon == "04d") {
-    img.setAttribute("src", "media/icon/partialy-cloudy.png");
-    div2.style.backgroundImage = "url('media/background/partlyCloudyDay.jpg')";
-  } else if (icon == "09d") {
-    img.setAttribute("src", "media/icon/light-rain.png");
-    div2.style.backgroundImage = "url('media/background/rainyDay.jpg')";
-  } else if (icon == "10d") {
-    img.setAttribute("src", "media/icon/heavy-rain.png");
-    div2.style.backgroundImage = "url('media/background/rainyDay.jpg')";
-  } else if (icon == "11d") {
-    img.setAttribute("src", "media/icon/thunder.png");
-    div2.style.backgroundImage = "url('media/background/thunderDay.jpg')";
-  } else if (icon == "13d") {
-    img.setAttribute("src", "media/icon/snow.png");
-    div2.style.backgroundImage = "url('media/background/snowyDay.jpg')";
-  } else if (icon == "50d") {
-    img.setAttribute("src", "media/icon/mist.png");
-    div2.style.backgroundImage = "url('media/background/mistyDay.jpg')";
-  }
-  div.appendChild(img);
+if(minute < 10)
+{
+  h3Time.innerHTML = `${hours}:0${minute}`;
+}
+else{
+  h3Time.innerHTML = `${hours}:${minute}`;
 }
 
+
+function getWeather(response) {
+    //Change the city name
+    /************************************************************/
+    let cityName = document.querySelector("#city-name");
+    cityName.innerHTML = response.data.name;
+    // gets current temp and displays it
+    /***********************************************************/
+    let currentTemp = Math.round(response.data.main.temp);
+    let h3Temp = document.querySelector("#current-temp");
+
+    h3Temp.innerHTML = `${currentTemp}`;
+    /************************************************************/
+
+    // gets weather info and displays it
+    /************************************************************/
+    let windspeed = document.querySelector("#wind");
+    let humidity = document.querySelector("#hum");
+    let highTemp = document.querySelector("#hi");
+    let lowTemp = document.querySelector("#low");
+
+    let currentWindspeed = Math.round(response.data.wind.speed);
+    let currentHumidity = response.data.main.humidity;
+    let currentHighTemp = Math.round(response.data.main.temp_max); 
+    let currentLowTemp = Math.round(response.data.main.temp_min);
+
+    windspeed.innerHTML = `<strong>Wind</strong>: ${currentWindspeed}km/hr`;
+    humidity.innerHTML = `<strong>Humidy</strong>: ${currentHumidity}%`;
+    highTemp.innerHTML = `<strong>High</strong>: ${currentHighTemp}°C`;
+    lowTemp.innerHTML = `<strong>Low</strong>: ${currentLowTemp}°C`;
+    /*****************************************************************/
+
+    // Changes weather icon according to the weather
+    /*****************************************************************/
+    let icon = response.data.weather[0].icon;
+    const image = document.querySelector("#icon");
+    const div2 = document.querySelector(".weather-app");
+  
+    if (icon == "01d") {
+      image.src="media/icon/sunny.png";
+      div2.style.backgroundImage = "url('media/background/sunnyDay.jpg')";  
+    } else if (icon == "02d") {
+      image.src="media/icon/sunny-cloud.png";
+      div2.style.backgroundImage = "url('media/background/scatteredCloudDay.jpg')";
+    } else if (icon == "03d") {
+      image.src="media/icon/cloudy.png";
+      div2.style.backgroundImage = "url('media/background/partlyCloudyDay.jpg')";
+    } else if (icon == "04d") {
+      image.src="media/icon/partly-cloudy.png";
+      div2.style.backgroundImage = "url('media/background/partlyCloudyDay.jpg')";
+    } else if (icon == "09d") {
+      image.src="media/icon/light-rain.png";
+      div2.style.backgroundImage = "url('media/background/rainyDay.jpg')";
+    } else if (icon == "10d") {
+      image.src="media/icon/heavy-rain.png";
+      div2.style.backgroundImage = "url('media/background/rainyDay.jpg')";
+    } else if (icon == "11d") {
+      image.src="media/icon/thunder.png";
+      div2.style.backgroundImage = "url('media/background/thunderDay.jpg')";
+    } else if (icon == "13d") {
+      image.src="media/icon/snow.png";
+      div2.style.backgroundImage = "url('media/background/snowyDay.jpg')";
+    } else if (icon == "50d") {
+      image.src="media/icon/mist.png";
+      div2.style.backgroundImage = "url('media/background/mistyDay.jpg')";
+    }
+    /*****************************************************************/
+  }
+function searchCity(event){
+  
+  let newCity = document.querySelector("#input-city").value;
+  let cityName = document.querySelector("#city-name");
+  
+  
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather); 
+  
+    
+}
+function clearInput() {
+  document.getElementById("#input-city").value = "";
+}
+
+function displayFah(event){
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let tempElement = document.querySelector("#current-temp");
+  let tempSymbol= document.querySelector("#symbol");
+  let celsiusTemp = tempElement.textContent;
+  let fahrenheitTemp = (celsiusTemp*9)/5+32;
+  tempElement.innerHTML = Math.round(fahrenheitTemp);
+  tempSymbol.innerHTML = "°F";
+
+}
+function displayCel(event){
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let tempElement = document.querySelector("#current-temp");
+  let tempSymbol = document.querySelector("#symbol");
+  let fahrenheitTemp = tempElement.textContent;
+  let celsiusTemp = (fahrenheitTemp -32) * 5 /9;
+  tempElement.innerHTML = Math.round(celsiusTemp);
+  tempSymbol.innerHTML = "°C";
+}
+let btnSearch = document.querySelector("#btn-search");
+btnSearch.addEventListener("click",searchCity);
 let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
 let city = "Cape Town"
 let currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(`${currentUrl}`).then(getWeather);
+
+let fahrenheitLink = document.querySelector("#fah-link");
+fahrenheitLink.addEventListener("click", displayFah);
+
+let celsiusLink = document.querySelector("#cel-link");
+celsiusLink.addEventListener("click", displayCel);
+
+
+
 
